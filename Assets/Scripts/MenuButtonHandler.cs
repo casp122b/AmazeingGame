f85503a1d664f2146ee5a4069da937fa0375f0a7
PBLauge <peder.laugesen@gmail.com>
2018-04-23 11:09:57 +0200

    Initial Commit
    Skeleton game with functional start menu

---
+++ Assets/Scripts/MenuButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonHandler : MonoBehaviour {
+
+    public void StartGameBtn(string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+    }
+
+    public void QuitGameBtn()
+    {
+        Application.Quit();
+    }
+}
