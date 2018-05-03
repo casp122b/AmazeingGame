145c85448221cc7a2895f91d52b6307887618ef4
Bunkern <kenneth@bunkern.dk>
2018-05-03 11:20:40 +0200

    Create User Scene works

--- Assets/Scripts/CharaterCreater.cs
+++ Assets/Scripts/CharaterCreater.cs
@@ -8,67 +8,20 @@ public class CharaterCreater : MonoBehaviour {
 
     public InputField firstNameField;
     public InputField lastNameField;
-    public InputField passwordField;
-    public InputField emailField;
     public InputField charaterNameField;
 
     private string firstName;
     private string lastName;
-    private string password;
-    private string email;
     private string charaterName;
-
-    private static int EMAIL_ERROR_CODE = 1;
-    private static int NO_ERRORS = 0;
 
     public void CreateCharater()
     {
-
-        int eCode = 0;
-        eCode = EmailValidator(emailField.text, eCode);
-        if (eCode == NO_ERRORS)        
-            
-        {
             firstName = firstNameField.text;
             lastName = lastNameField.text;
-            password = passwordField.text;
-            email = emailField.text;
             charaterName = charaterNameField.text;
 
             Debug.Log("First Name" + firstName + "Last Name" + lastName);
-            Debug.Log("Password" + password + "Email" + email);
             Debug.Log("Charater Name" + charaterName);
-        }
-        else if (eCode == EMAIL_ERROR_CODE)
-        {
-            Debug.Log("Invalidate Email");
-        }
-    }
 
-    private int EmailValidator(string emailcheck, int crnECode)
-    {
-        if (!emailcheck.Contains("@") && !emailcheck.Contains("."))
-        {
-            return EMAIL_ERROR_CODE;
-        }
-            return crnECode;
-    }
-
-    public void ButtonPressed()
-    {
-        FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(emailField.text, passwordField.text).
-            ContinueWith((GameObject) => 
-            {
-                SceneManager.LoadSceneAsync("ProfileScene");
-            });
-    }
-    public void CreateUserButton()
-    {
-        FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(emailField.text, passwordField.text).
-            ContinueWith((GameObject) => 
-            {
-                CreateCharater();
-            SceneManager.LoadSceneAsync("Level1");
-            });
     }
 }
