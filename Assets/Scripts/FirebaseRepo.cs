dd52b99238f1f8d94f8348923279abe702ffa01f
Bunkern <kenneth@bunkern.dk>
2018-05-07 15:00:06 +0200

    Database Connection startet

---
+++ Assets/Scripts/FirebaseRepo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+
+[CreateAssetMenu(menuName ="Firebase/Repository")]
+public class FirebaseRepo : ScriptableObject {
+
+    //public FirebasePath Path;
+
+
+    //public Task Push(object item)
+    //{
+    //    if(Firebase.Auth.FirebaseAuth.DefaultInstance.CurrentUser != null)
+    //    {
+    //        return Path.GetReferenceFromRoot(Firebase.Database.FirebaseDatabaseS)
+    //    }
+    //}
+}
