2124b9a912f51e796eec60cc522a530466b39781
Bunkern <kenneth@bunkern.dk>
2018-05-08 12:43:12 +0200

    Making opath to Database

---
+++ Assets/Scripts/FirebasePath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Firebase/Path")]
+public class FirebasePath : ScriptableObject
+{
+
+    public string BasePath, DBVersion, ObjectTypeName;
+    public bool IsPlural;
+
+    private string FullPath { get { return DBVersion + "/" + BasePath + "/" + ObjectTypeName + (IsPlural ? "s" : ""); } }
+
+    public Firebase.Database.DatabaseReference GetReferenceFromRoot (Firebase.Database.DatabaseReference root)
+    {
+        var objectTypeName = ObjectTypeName + (IsPlural ? "s" : "");
+        return root.Child(DBVersion).Child(BasePath).Child(ObjectTypeName);
+    }
+}
