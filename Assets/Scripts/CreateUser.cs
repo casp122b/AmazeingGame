52575f84f353e3ab0fd8ad2adb2a8aee4fa88c97
Bunkern <kenneth@bunkern.dk>
2018-05-14 11:41:59 +0200

    Login work - start scene changed

--- Assets/Scripts/CreateUser.cs
+++ Assets/Scripts/CreateUser.cs
@@ -3,6 +3,7 @@ using Firebase;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Firebase.Unity.Editor;
 
 public class CreateUser : MonoBehaviour {
 
@@ -14,7 +15,13 @@ public class CreateUser : MonoBehaviour {
     private static int EMAIL_ERROR_CODE = 1;
     private static int NO_ERRORS = 0;
 
-    public void CreateCharater()
+    void Start()
+    {
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://a-maze-inggladiator.firebaseio.com/");
+    }
+
+    public void ValidateEmail()
     {
 
         int eCode = 0;
@@ -42,21 +49,48 @@ public class CreateUser : MonoBehaviour {
             return crnECode;
     }
 
-    public void ButtonPressed()
+    public void LogInButton()
     {
         FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(emailField.text, passwordField.text).
-            ContinueWith((GameObject) => 
+            ContinueWith(task => 
             {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+                //ValidateEmail();
+                FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
                 SceneManager.LoadSceneAsync("ProfileScene");
             });
     }
     public void CreateUserButton()
     {
         FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(emailField.text, passwordField.text).
-            ContinueWith((GameObject) => 
+            ContinueWith(task =>
             {
-                CreateCharater();
-            SceneManager.LoadSceneAsync("ProfileScene");
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+                //ValidateEmail();
+                FirebaseUser newUser = task.Result;
+                Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+                SceneManager.LoadSceneAsync("ProfileScene");
             });
     }
 }
